c0246330a11ec53f138dba664f139094abf1c80c
Denis <denis-velo.pro@mail.ru>
2024-06-08 22:25:32 +0500

    fix bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @ts-ignore
 export default defineNuxtConfig({
+  base: '/nuxt-apartments/',
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@formkit/auto-animate"]
 })
